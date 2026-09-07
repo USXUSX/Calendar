3c2f44914680d33f66807411241a1ee0ba084bf0
us <39054159+USXUSX@users.noreply.github.com>
2026-09-07 11:19:38 +0900

    Attach transient weather context to CAL detail views (#95)

--- Sources/calendar_domain/__init__.py
+++ Sources/calendar_domain/__init__.py
@@ -2,8 +2,31 @@
 
 from .errors import ConflictError, DomainError, GenerationWriteError, NotFoundError, ValidationError
 from .models import UnifiedEvent
-from .service import CalendarDomain
+from .service import CalendarDomain as _CalendarDomain
 from .trip_detail import build_local_ai_update_request, build_trip_detail_view
+from .weather import OpenMeteoAdapter, build_weather_by_day
+
+
+class CalendarDomain(_CalendarDomain):
+    """Calendar domain with transient Goal 1 weather context on ordinary detail views."""
+
+    def __init__(self, db_path, trip_root, *, weather_adapter=None, weather_today=None):
+        super().__init__(db_path, trip_root)
+        self._weather_adapter = weather_adapter or OpenMeteoAdapter()
+        self._weather_today = weather_today
+
+    def get_trip_detail_view(self, trip_id, *, candidate_judgments=None, weather_by_day=None):
+        effective = self.get_effective_trip(trip_id)
+        if weather_by_day is None:
+            weather_by_day = build_weather_by_day(
+                effective, self._weather_adapter, today=self._weather_today,
+            )
+        return build_trip_detail_view(
+            effective,
+            candidate_judgments=candidate_judgments,
+            weather_by_day=weather_by_day,
+        )
+
 
 __all__ = [
     "CalendarDomain",
@@ -11,8 +34,10 @@ __all__ = [
     "DomainError",
     "GenerationWriteError",
     "NotFoundError",
+    "OpenMeteoAdapter",
     "UnifiedEvent",
     "ValidationError",
     "build_local_ai_update_request",
     "build_trip_detail_view",
+    "build_weather_by_day",
 ]
